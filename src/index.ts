6208f9288171bb00c190d3025936a1c847c39cd8
Chinsan Chung <chinsanchung@gmail.com>
2021-11-03 02:40:10 +0900

    chore: 빌드를 위한 설정
    - npm start 스크립트 수정
    - process.env.NODE_PORT => process.env.PORT
    
    resolves: #1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import app from "./core/app";
-import database from "./core/database";
-import dotenv from "dotenv";
-import Log from "./utils/debugger";
+import app from './core/app';
+import database from './core/database';
+import dotenv from 'dotenv';
+import Log from './utils/debugger';
 
 dotenv.config();
 
 database(process.env.MONGO_URI).then(() => {
   // server
   const options = {
-    port: process.env.NODE_PORT || 3000,
+    port: process.env.PORT || 3000,
   };
 
   app.listen(options, () => Log.info(`server on!!!${options.port}`));
